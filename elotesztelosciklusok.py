ed9cf8315601e980e974ba39b219c650f5fd6297
odoroliver1 <71767269+odoroliver1@users.noreply.github.com>
2021-10-21 09:40:53 +0200

    Add files via upload

---
+++ elotesztelosciklusok.py
@@ -0,0 +1,27 @@
+#24.f
+def feladat24():
+    szam = float(input("Kérek egy számot: "))
+    while szam!=0:
+        szam = float(input("Kérek egy másik számot: "))
+
+#itt kezdődik a főprogram
+
+#25.f
+def feladat25():
+    szam = float(input("Kérek egy pozitív számot: "))
+    while szam<=0:
+        print("Nem pozitiv szamot adtal, add meg ujra")
+
+#26.f
+def feladat26():
+    szam = float(input("Kérek egy számot: "))
+    while szam<=10:
+        szam = float(input("Kérek egy másik számot: "))
+        osszeg +=szam:
+        print("A számok összege", osszeg)
+
+    
+
+#feladat24()
+#feladat25()
+feladat26()
